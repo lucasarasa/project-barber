2f207510bd7d67374a2ce65a99b0953b0e1c7ae1
Lucas Sarasa <lucasmsarasa@gmail.com>
2025-09-18 19:11:39 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 // SERVER COMPONENTS
 const Home = () => {
@@ -10,15 +13,19 @@ const Home = () => {
     <div>
       <Header />
       <div className="p-5">
+        {/* texto */}
         <h2 className="text-xl font-bold">Olá, Lucas</h2>
         <p>Segunda-feira, 5 de agosto.</p>
 
+        {/* busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
+
+        {/* imagem */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -27,6 +34,29 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* agendamento */}
+        <Card className="mt-6 p-0">
+          <CardContent className="flex justify-between p-0">
+            {/* div esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* div direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
