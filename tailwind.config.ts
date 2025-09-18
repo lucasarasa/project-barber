a6c4cd8eb1010e577da18af4760934956cda103e
Lucas Sarasa <lucasmsarasa@gmail.com>
2025-09-18 16:39:06 -0300

    feat(deps): update dependency versions
    BREAKING CHANGE: updated major versions of Tailwind and Next.js to match professor's setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "tailwindcss"
+import tailwindAnimate from "tailwindcss-animate"
 
 const config = {
-  darkMode: "class",
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -74,7 +75,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 } satisfies Config
 
 export default config
