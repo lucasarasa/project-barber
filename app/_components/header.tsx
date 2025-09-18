b1230dc89c5d4cf672d1fbfeed10af96ee8087bc
Lucas Sarasa <lucasmsarasa@gmail.com>
2025-09-18 16:39:50 -0300

    feat: add Header component and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+const Header = () => {
+  return (
+    <div>
+      <Card>
+        <CardContent className="flex flex-row items-center justify-between p-4">
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default Header
