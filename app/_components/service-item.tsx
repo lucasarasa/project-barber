3b8fc535900fd3096187d9adb11464d97adefb68
Lucas Sarasa <lucasmsarasa@gmail.com>
2025-09-23 16:52:38 -0300

    feat: ensure booking details can be viewed

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -239,14 +239,12 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
                           {/* Horario */}
                           <div className="flex items-center justify-between">
                             <h2 className="text-sm text-gray-400">Horário</h2>
-                            <p className="text-sm font-bold">{selectedTime}</p>
+                            <p className="text-sm">{selectedTime}</p>
                           </div>
 
                           <div className="flex items-center justify-between">
                             <h2 className="text-sm text-gray-400">Barbearia</h2>
-                            <p className="text-sm font-bold">
-                              {barbershop.name}
-                            </p>
+                            <p className="text-sm">{barbershop.name}</p>
                           </div>
                         </CardContent>
                       </Card>
